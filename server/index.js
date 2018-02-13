70088027fa7c1b0a2855d08c8a8895d5004f6969
unknown <jordanmbush@gmail.com>
2018-02-13 13:35:29 -0700

    initial commit, completed mini

--- server/index.js
+++ server/index.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mc = require( `./controllers/messages_controller` );
+const session = require('express-session');
+require('dotenv').config();
+const { createInitialSession } = require('./middlewares/session');
+const filter = require('./middlewares/filter');
 
 const app = express();
 
 app.use( bodyParser.json() );
 app.use( express.static( `${__dirname}/../build` ) );
 
+app.use( session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        // TWO WEEKS
+        age: 10000
+    }
+}))
+
+app.use( createInitialSession );
+
 const messagesBaseUrl = "/api/messages";
-app.post( messagesBaseUrl, mc.create );
+app.post( messagesBaseUrl, filter, mc.create );
 app.get( messagesBaseUrl, mc.read );
-app.put( `${messagesBaseUrl}`, mc.update );
+app.get( `${messagesBaseUrl}/history`, mc.history );
+app.put( `${messagesBaseUrl}`, filter, mc.update );
 app.delete( `${messagesBaseUrl}`, mc.delete );
 
 const port = process.env.PORT || 3000
